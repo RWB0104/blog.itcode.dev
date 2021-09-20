25a5625e8c56834978ee954a7f4a0e23fec998e7
RWB0104 <psj1789@naver.com>
2021-09-20 22:46:19 +0900

    빌드 f1814897788 버전 d7daa05 커밋 발행

---
+++ docs/js/ga.js
@@ -0,0 +1,19 @@
+/**
+ * Google Analytics 자바스크립트
+ *
+ * @author RWB
+ * @since 2021.08.13 Fri 21:50:32
+ */
+
+/**
+ * GA 동작 함수
+ */
+function gtag()
+{
+	dataLayer.push(arguments);
+}
+
+window.dataLayer = window.dataLayer || [];
+
+gtag('js', new Date());
+gtag('config', 'G-7QYWYNZ90R');

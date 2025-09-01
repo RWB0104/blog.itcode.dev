1eb7fa013724dce1eaf616c1cd8e4eb8e9ebfcd7
RWB0104 <psj2716@gmail.com>
2025-09-01 09:02:51 +0000

    release: 🚀 @kapoo/blog v6.8.0

---
+++ docs/_next/static/KrLuqwksNwf-tHm0LKHWD/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/c94ad9c0fbb5f2e2.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/chunks/c94ad9c0fbb5f2e2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/578d9ff13a2bf4cb.js",
+  "static/chunks/028c51c52bf29925.js",
+  "static/chunks/turbopack-c79bfa8879d5567d.js"
+])
